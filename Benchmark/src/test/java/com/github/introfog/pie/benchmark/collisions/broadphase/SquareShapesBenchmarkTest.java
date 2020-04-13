32ba90cb7bf991c8629626388407aad99d2dbe6d
Dmitry Chubrick <chubrickdm@gmail.com>
2020-04-13 20:12:36 +0300

    #21 prepare project to uploading in Nexus
    * #21 Update groupId and add required info in pom files
    
    * #21 Add builds and compile plugins and add minor improvements
    
    * #21 Update source folder paths and add minor improvements
    
    * #21 Add file minor improvements
    
    * #21 Add unit categories, PIETest class and minor improvements
    
    * #21 Add release and deploying plugins
    
    * #21 Add minor improvements

---
+++ Benchmark/src/test/java/com/github/introfog/pie/benchmark/collisions/broadphase/SquareShapesBenchmarkTest.java
@@ -0,0 +1,53 @@
+/*
+   Copyright 2020 Dmitry Chubrick
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+package com.github.introfog.pie.benchmark.collisions.broadphase;
+
+import com.github.introfog.pie.test.annotations.BenchmarkTest;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@Category(BenchmarkTest.class)
+public class SquareShapesBenchmarkTest extends AbstractBroadPhaseBenchmarkTest {
+    private final static String PATH_TO_SOURCE_FOLDER = "./src/test/resources/com/github/introfog/pie/benchmark/collisions/broadphase/Square/";
+
+    @Test
+    public void simpleSquare() throws IOException {
+        super.runBenchmarkTest("50x50square_9702collision.pie", PATH_TO_SOURCE_FOLDER);
+    }
+
+    @Test
+    public void mediumSquare() throws IOException {
+        super.runBenchmarkTest("50x50square_28518collision.pie", PATH_TO_SOURCE_FOLDER);
+    }
+
+    @Test
+    public void mediumSquareWithDiffSize() throws IOException {
+        super.runBenchmarkTest("70x70square+diffSize_17320collision.pie", PATH_TO_SOURCE_FOLDER);
+    }
+
+    @Test
+    public void simpleSquareScatteredWithDiffSize() throws IOException {
+        super.runBenchmarkTest("100x100square+scattered+diffSize_14344collision.pie", PATH_TO_SOURCE_FOLDER);
+    }
+
+    @Test
+    public void simpleSquareScattered() throws IOException {
+        super.runBenchmarkTest("100x100square+scattered_14602collision.pie", PATH_TO_SOURCE_FOLDER);
+    }
+}
